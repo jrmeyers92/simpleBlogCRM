afe8f559c1030c26eafaeccf45875a34b52872f2
Jake Meyers <jakejake92@yahoo.com>
2020-12-27 14:08:48 -0700

    created fucntions to create post and push post to array

--- script.js
+++ script.js
@@ -9,13 +9,30 @@ function BlogPost(blogTitle, blogSubTitle, blogAuthor, blogContent) {
 	this.blogContent = blogContent;
 }
 
-//event listener on add post form submit
-addPostForm.addEventListener("submit", () => {
+//function to create object for new blog post
+const createNewBlogPost = () => {
 	const blogTitle = document.getElementById("postTitle");
 	const blogSubTitle = document.getElementById("postSubTitle");
 	const blogAuthor = document.getElementById("postAuthor");
-	const blogContent = document.getElementById("postText");
+	const blogContent = document.getElementById("postContent");
 
-    
-    
+	let post = new BlogPost(
+		blogTitle.value,
+		blogSubTitle.value,
+		blogAuthor.value,
+		blogContent.value
+	);
+	return post;
+};
+
+// function to push object to array
+const pushArray = (object, array) => {
+	array.push(object);
+};
+
+//event listener on add post form submit
+addPostForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+	pushArray(createNewBlogPost(), blogPosts);
+
 });
